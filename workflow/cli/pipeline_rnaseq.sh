4e3e7e292c670ac716f9eea724a55942170cd1fa
Jérémy <94016535+jroussea@users.noreply.github.com>
2024-05-03 13:50:51 +0200

    Update pipeline_rnaseq.sh

--- workflow/cli/pipeline_rnaseq.sh
+++ workflow/cli/pipeline_rnaseq.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-date 
+start=`date`
 
 genome="`pwd`/data/reference/genome.fa"
 reads_1="`pwd`/data/reads/SRR6357070_1.fastq.gz"
@@ -12,24 +12,22 @@ wkdir="`pwd`/wdir"
 # results folder
 mkdir results
 
-#CONDA environment
-#echo "Build environment"
-#conda env create -f environment.yml
-#conda activate pipeline
-
 # identifiant
 id=`echo $reads_1 | cut -d "_" -f 1`
 
-# indéxation du génome
 mkdir -p genome_dir
 
+echo "------------"
 echo "Genome index"
+echo "------------"
 STAR --runMode genomeGenerate \
      --genomeDir genome_dir \
      --genomeFastaFiles $genome \
      --runThreadN 8
 
+echo "----------------"
 echo "Genome alignment"
+echo "----------------"
 STAR --genomeDir genome_dir \
      --readFilesIn $reads_1 $reads_2 \
      --runThreadN 8 \
@@ -38,21 +36,30 @@ STAR --genomeDir genome_dir \
      --alignSJoverhangMin 8 \
      --alignSJDBoverhangMin 1 \
      --outFilterMismatchNmax 999 \
-     --outSAMtype BAM Aligned.SortedByCoordinate \
+     --outSAMtype BAM SortedByCoordinate \
      --outSAMattrRGline ID:$id LB:library PL:illumina PU:machine SM:GM12878
 
+echo "---------"
 echo "Bam index"
-samtools index SortedByCoordinate
+echo "---------"
+samtools index Aligned.sortedByCoord.out.bam
 
+echo "-----------------"
 echo "Quality alignment"
-qualimap rnaseq -bam Aligned.SortedByCoordinate -gtf $gtf -outdir $results/$id-qualimap-report --java-mem-size=16G
+echo "-----------------"
+qualimap rnaseq -bam Aligned.sortedByCoord.out.bam. -gtf $gtf -outdir $results/$id-qualimap-report --java-mem-size=16G
 
+echo "-----------"
 echo "Count reads"
+echo "-----------"
 featureCounts -p -s 0 \
               -t exon \
               -g gene_id \
-              -a $gtf} \
+              -a $gtf \
               -o $results/$id_count.txt \
-              Aligned.SortedByCoordinate
+              Aligned.sortedByCoord.out.bam
 
-#conda deactivate
+end=`date`
+
+echo "start: "$start
+echo "end :"$end
